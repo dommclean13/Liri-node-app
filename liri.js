435f70580f4591ff7fbfcf068913d540481e2437
Dominique McLean <dommclean13@gmail.com>
2019-11-12 17:13:52 -0600

    added more info

--- liri.js
+++ liri.js
@@ -1,16 +1,37 @@
+// pulling in spotify keys so that outside sources don't have access to my personal key
 require("dotenv").config();
-// pulling in api keys
+
+// pulling in api keys and linking other files to access the npms
 let keys = require("./keys.js");
+let fs = require(fs);
+let axios = require("axios");
+let Spotify = require("node-spotify-api");
 let spotify = new Spotify(keys.spotify);
- 
 
-let getMySong = function() {
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+// gets user input
+let userArg = process.argv[2]; 
+let inputParam = process.argv[3];
 
-};
+
+// UserInputs(userArg, inputParam);
+
+
+//function that will execute our requests
+function UserInputs (userArg, inputParam){
+    switch (userArg) {
+    case 'concert-this':
+        pullConcerts(inputParam);
+        break;
+    case 'spotify-this-song':
+        pullSongs(inputParam);
+        break;
+    case 'movie-this':
+        pullMovies(inputParam);
+        break;
+    case 'do-what-it-says':
+        pullInfo();
+        break;
+    default: 
+        console.log("Invalid input. You must type: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
+}
